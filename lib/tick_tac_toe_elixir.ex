6c6d276993e229f35d4ec5657c97e7f06b7c5c2f
Jakub Hebda <hebdajakub2aizst@gmail.com>
2023-04-10 18:33:57 +0100

    Tic Tac Toe project initialization

---
+++ lib/tick_tac_toe_elixir.ex
@@ -0,0 +1,18 @@
+defmodule TickTacToeElixir do
+  @moduledoc """
+  Documentation for `TickTacToeElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TickTacToeElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tick_tac_toe_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule TickTacToeElixirTest do
+  use ExUnit.Case
+  doctest TickTacToeElixir
+
+  test "greets the world" do
+    assert TickTacToeElixir.hello() == :world
+  end
+end
