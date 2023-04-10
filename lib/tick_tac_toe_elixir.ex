a7e24c8cab9523f025cfa080ed24f317bde1ef3b
Jakub Hebda <hebdajakub2aizst@gmail.com>
2023-04-10 18:40:53 +0100

    Added functionality of drawing the board and drawing board with example case

--- lib/tick_tac_toe_elixir.ex
+++ lib/tick_tac_toe_elixir.ex
@@ -1,18 +1,44 @@
-defmodule TickTacToeElixir do
-  @moduledoc """
-  Documentation for `TickTacToeElixir`.
-  """
+defmodule Ttt do
+  def new_board do
+    for col <- 1..3,
+        row <- 1..3,
+        into: %{},
+        do: {{col, row}, " "}
+  end
 
-  @doc """
-  Hello world.
+  def put(board, {col, row}, val) do
+    put(board, col, row, val)
+  end
 
-  ## Examples
+  def put(board, col, row, val), do: Map.put(board, {col, row}, val)
 
-      iex> TickTacToeElixir.hello()
-      :world
+  def fill(board, list, which) do
+    Enum.reduce(list, board, fn point, acc -> put(acc, point, which) end)
+  end
 
-  """
-  def hello do
-    :world
+  def fill_example_1(board) do
+    xs = [{1, 1}, {2, 2}, {3, 3}]
+    ys = [{2, 1}, {3, 1}, {1, 2}]
+
+    board
+    |> fill(xs, "X")
+    |> fill(ys, "O")
+  end
+
+  def print(board) do
+    for row <- 1..3 do
+      for col <- 1..3 do
+        " " <> board[{col, row}]
+      end
+      |> Enum.join(" |")
+    end
+    |> Enum.join("\n---+---+---\n")
+    |> IO.puts()
+  end
+
+  def run do
+    new_board()
+    |> fill_example_1()
+    |> print
   end
 end
